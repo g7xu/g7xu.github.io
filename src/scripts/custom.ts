b2c51ac857bd601e6f6f53462c7b1a5298851342
Jason Xu <guoxuan.xu8@gmail.com>
2026-02-25 21:58:33 -0800

    Create custom.ts

---
+++ src/scripts/custom.ts
@@ -0,0 +1,31 @@
+// Typing effect for homepage title
+const pageTitle = document.querySelector("h1");
+if (
+  pageTitle &&
+  pageTitle.textContent &&
+  pageTitle.textContent.length > 0 &&
+  (window.location.pathname === "/" ||
+    window.location.pathname === "/index.html")
+) {
+  if (
+    pageTitle.textContent.includes("About me") ||
+    pageTitle.textContent.includes("Home")
+  ) {
+    const originalText = pageTitle.textContent;
+    pageTitle.textContent = "";
+    pageTitle.style.borderRight = "2px solid #007acc";
+
+    let i = 0;
+    const typeWriter = () => {
+      if (i < originalText.length) {
+        pageTitle.textContent += originalText.charAt(i);
+        i++;
+        setTimeout(typeWriter, 100);
+      } else {
+        pageTitle.style.borderRight = "none";
+      }
+    };
+
+    typeWriter();
+  }
+}
